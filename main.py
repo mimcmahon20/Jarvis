614694fc895533421bae6fc76d5b8d12bac0a541
mimcmahon20 <mimcmahon20@vt.edu>
2024-01-02 12:36:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,35 +9,6 @@ import jarvis.components.command_listener as command_listener
 import jarvis.components.wake_word_listener as wake_word_listener
 from jarvis.gui.gui import JarvisWindow
 
-
-'''
-# Create the visual indicator instance
-indicator = VisualIndicator()
-
-# Define GUI update functions
-def update_gui(mode):
-    # This function updates the GUI in a thread-safe manner.
-    def change_mode():
-        indicator.change_mode(mode)
-        indicator.show() if mode != 'hide' else indicator.hide()
-
-    indicator.root.after(0, change_mode)
-
-speech_output.set_update_gui_function(update_gui)
-command_listener.set_update_gui_function(update_gui)
-
-
-
-def toggle_gui():
-    if indicator.root.state() == 'normal':
-        indicator.hide()
-    else:
-        indicator.show()
-
-speech_output.set_toggle_gui_function(toggle_gui)
-wake_word_listener.set_toggle_gui_function(toggle_gui)
-'''
-
 # Initialize Jarvis Core
 jarvis_core = JarvisCore('DZzwIYi6/ckJ66kdntAOPfjmzl9iLhYJChPDMOvMaTaeFQMEcIWtHQ==')
 
